d528b05434da78d2d02472306b38a8382078f222
Jasam-Kajam <samuelwekesa24@gmail.com>
2025-07-17 15:05:49 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
-// Your web app's Firebase configuration
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyAwAxP-MWwNnKP59PA6joil4Ceq10eozlc",
   authDomain: "wafula-s-educational-posts.firebaseapp.com",
-  databaseURL: "https://wafula-s-educational-posts-default-rtdb.firebaseio.com",
   projectId: "wafula-s-educational-posts",
-  storageBucket: "wafula-s-educational-posts.firebasestorage.app",
+  storageBucket: "wafula-s-educational-posts.appspot.com",
   messagingSenderId: "1040710248503",
   appId: "1:1040710248503:web:87fbd02de2b919511577b2"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
